08edb6277177a200403217b6d368ac6c11062480
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-07-23 19:17:05 -0700

    initial vite commit

---
+++ src/components/hero.jsx
@@ -0,0 +1,7 @@
+const hero = () => {
+  return (
+    <div className="border-b border-neutral-900">hero</div>
+  )
+}
+
+export default Hero;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      scale: {
+        '30': '0.30',
+      }
+    },
+  },
+  plugins: [],
+}
