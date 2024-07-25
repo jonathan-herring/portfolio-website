63582e7cab9986a4bb8c2d33a8b69d828c8ed3be
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-07-25 12:44:20 -0700

    chore: Add framer-motion library for animations and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Technologies from './components/Technologies';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,15 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/jonathanHerringProfile.jpg"
+import profilePic from "../assets/jonathanHerringProfile.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: {x: -100, opacity: 0},
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay }
+  }
+});
 
 const Hero = () => {
   return (
@@ -7,21 +17,43 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
             <div className="flex flex-col items-center lg:items-start">
-              <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              <motion.h1 
+                variants={container(0)}
+                initial="hidden"
+                animate="visible"
+                className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              >
                 Jonathan Herring
-              </h1>
-              <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
-              bg-clip-text text-4xl tracking-tight text-transparent">
+              </motion.h1>
+              <motion.span
+                variants={container(0.1)}
+                initial="hidden"
+                animate="visible" 
+                className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
+                bg-clip-text text-3xl tracking-tight text-transparent"
+              >
                 Computer Science Student
-              </span>
-              <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              </motion.span>
+              <motion.p
+                variants={container(0.2)}
+                initial="hidden"
+                animate="visible"
+                className="my-2 max-w-xl py-6 font-light tracking-tighter"
+              >
                 {HERO_CONTENT}
-              </p>
+              </motion.p>
             </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="Picture of Jonathan Herring" className="w-1/2 h-auto rounded-md" />
+            <motion.img 
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 0.5}}
+              src={profilePic} 
+              alt="Picture of Jonathan Herring" 
+              className="w-1/2 h-auto rounded-md" 
+            />
           </div>
         </div>
       </div>
